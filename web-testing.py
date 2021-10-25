6a908e4df5dc6447e6106e0c663ac260f16d63d7
Arturo0911 <anegreiross@outlook.com>
2021-10-25 02:03:10 -0500

    ok

--- web-testing.py
+++ web-testing.py
@@ -2,6 +2,9 @@
 import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
 
 class HomePageTests(unittest.TestCase):
 
@@ -15,9 +18,25 @@ class HomePageTests(unittest.TestCase):
         
     def test_search_text_field(self):
         search_field = self.driver.find_element_by_id("search")
+        
+    def test_search_text_field_by_name(self):
+        search_field = self.driver.find_element_by_name("q")
     
     def tearDown(self):
         self.driver.quit()
+    
+    def test_search_field(self):
+        self.assertTrue(self.is_element_present(By.NAME, 'q'))
+    
+    def test_language_option(self):
+        self.assertTrue(self.is_element_present(By.ID, 'select-language'))
+        
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
 
 
 def main():
