83c8899e46a67f904e566b7249a9ed91bff25dc6
Arturo0911 <anegreiross@outlook.com>
2021-10-27 00:16:42 -0500

    ok

---
+++ PyDorks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+class GoogleDork:
+    Url = "www.google.com/search/"
+    inTitle = "intitle:"
+    allinText = "allintext:"
+    inUrl = "inurl:"
+    site = "site:"
+    fileType = "filetype:"
+    link = "link:"
+    cache = "cache:"
+    info = "info:"
+
+
+class ImplementDorks:
+
+    def create_query(self):
+        """"""
