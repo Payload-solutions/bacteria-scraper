ca57dcde5ef635329a038ad3ddf72b08f776568d
Arturo0911 <anegreiross@outlook.com>
2021-10-26 21:12:47 -0500

    ok

---
+++ smoketests.py
@@ -0,0 +1,17 @@
+from unittest import (
+    TestLoader,
+    TestSuite
+)
+
+from pyunitreport import HTMLTestRunner
+from assertions import HomePageTests
+
+
+assertions_tests = TestLoader().loadTestsFromTestCase(HomePageTests)
+smoke_test = TestSuite([assertions_tests])
+
+kwargs = {
+    "output":'smoke-report'
+}
+runner = HTMLTestRunner(**kwargs)
+runner.run(smoke_test)
