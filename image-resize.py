bd4d9deaa1e674b048cce0735057c1026ea3f9c7
Arturo0911 <anegreiross@outlook.com>
2021-11-03 13:59:01 -0500

    moved the images folder to start with the trainiung

--- image-resize.py
+++ image-resize.py
@@ -5,20 +5,22 @@ import os
 from pprint import pprint
 
 
-BASE_DIR = "lactobacillus/"
+BASE_DIR = "streptococcus/"
 NEW_DIR = "images/"
 
 
 def main():
     # base = os.path.join(BASE_DIR)
-    base = [data for data in os.listdir(NEW_DIR)]
+    base = [data for data in os.listdir(BASE_DIR)]
     for pos, x in enumerate(base):
-        read_image = Image.open(os.path.join(NEW_DIR, x))
+        read_image = Image.open(os.path.join(BASE_DIR, x))
         # print(read_image)
-        name = f"{NEW_DIR}lactobacillus_{pos+len(base)}.png"
+        name = f"{NEW_DIR}streptococcus_{pos+2+len(base)}.png"
         
-        img_format = read_image.format
-        new_image = read_image.rotate(180)
+        # img_format = read_image.format
+        # new_image = read_image.resize((64,64))
+        # new_image = read_image.convert('L')
+        new_image = read_image.rotate(90)
         new_image.save(name)
 
     # pprint(base)
